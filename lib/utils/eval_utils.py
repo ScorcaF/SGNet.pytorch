dc8e3ca5696b8b0ca2a9f721e31b3d9d6ec824ea
francesco scorca <70110839+ScorcaF@users.noreply.github.com>
2022-10-24 15:35:50 +0200

    Update eval_utils.py

--- lib/utils/eval_utils.py
+++ lib/utils/eval_utils.py
@@ -2,7 +2,6 @@ import os
 import json
 import numpy as np
 from .data_utils import bbox_denormalize, cxcywh_to_x1y1x2y2
-from nuscenes.prediction import convert_local_coords_to_global
 def compute_IOU(bbox_true, bbox_pred, format='xywh'):
     '''
     compute IOU
@@ -171,48 +170,3 @@ def eval_ethucy_cvae(input_traj, target_traj, cvae_all_traj):
 
 
     return result
-
-def eval_nuscenes_local(starting_translation, starting_rotation, target_traj, cvae_all_traj):
-    result = {'ADE_12':0, 'FDE_12':0}
-
-
-    K = cvae_all_traj.shape[2]
-    B = cvae_all_traj.shape[0]
-    tiled_target_traj = np.tile(target_traj[:, :, None, :], (1, 1, K, 1))
-    
-    cvae_all_traj_global = np.zeros(cvae_all_traj.shape)
-    for k in range(K):
-        for b in range(B):
-            cvae_all_traj_global[b,:,k,:] = convert_local_coords_to_global(cvae_all_traj[b,:,k,:],starting_translation[b] ,starting_rotation[b]) 
-    result['ADE_12'] = np.linalg.norm(cvae_all_traj_global[:,:12,:,:] - tiled_target_traj[:,:12,:,:], axis=-1).mean(axis=1).min(axis=1).sum()
-    result['FDE_12'] = np.linalg.norm(cvae_all_traj_global[:,11,:,:] - tiled_target_traj[:,11,:,:], axis=-1).min(axis=1).sum()
-
-
-    return result
-
-
-
-def eval_nuscenes_api(starting_translation, starting_rotation, target_traj, cvae_all_traj, total_probabilities, tokens):
-    result = {'ADE_12':0, 'FDE_12':0}
-
-
-    K = cvae_all_traj.shape[2]
-    B = cvae_all_traj.shape[0]
-    tiled_target_traj = np.tile(target_traj[:, :, None, :], (1, 1, K, 1))
-    preds5 = []
-    cvae_all_traj_global = np.zeros(cvae_all_traj.shape)
-    for k in range(K):
-        for b in range(B):
-            cvae_all_traj_global[b,:,k,:] = convert_local_coords_to_global(cvae_all_traj[b,:,k,:],starting_translation[b] ,starting_rotation[b])
-    
-    cvae_all_traj_global = np.transpose(cvae_all_traj_global, (0,2,1,3))
-
-    tiled_target_traj = np.transpose(tiled_target_traj, (0,2,1,3))
-    for i, token in enumerate(tokens):
-        
-        instance_token, sample_token = token.split("_")
-        prediction = Prediction(instance=instance_token, sample=sample_token, prediction=cvae_all_traj_global[i],
-                                        probabilities=total_probabilities[i]).serialize()
-        preds5.append(prediction)
-
-    return preds5
