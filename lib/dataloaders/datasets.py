d7f92d88b7dda0c340b29be3c1cf057b163b3ff4
francesco scorca <70110839+ScorcaF@users.noreply.github.com>
2022-10-25 18:14:12 +0200

    Update datasets.py

--- lib/dataloaders/datasets.py
+++ lib/dataloaders/datasets.py
@@ -1,6 +1,8 @@
 from .jaad_data_layer import JAADDataLayer
 from .pie_data_layer import PIEDataLayer
 from .ethucy_data_layer import ETHUCYDataLayer
+from .sensor_data_layer import SENSORDataLayer
+
 
 def build_dataset(args, phase):
     print(args.dataset)
@@ -10,4 +12,6 @@ def build_dataset(args, phase):
         data_layer = PIEDataLayer
     elif args.dataset in ['ETH', 'HOTEL','UNIV', 'ZARA1', 'ZARA2']:
         data_layer = ETHUCYDataLayer
+    elif args.dataset in ['SENSOR']:
+        data_layer = SENSORDataLayer
     return data_layer(args, phase)
